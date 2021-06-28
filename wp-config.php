f931802342e43d86b9cce9c05c4335cc653d81d5
JSebastian-C <86528745+JSebastian-C@users.noreply.github.com>
2021-06-27 20:24:35 -0500

    -

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'myappi_markoh' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', ' ' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
